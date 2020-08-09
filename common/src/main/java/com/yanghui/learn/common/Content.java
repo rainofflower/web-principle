ca257bdbe55e78ec27456b243d5f48edf7615e7c
rainofflower <rainofflower.yh@qq.com>
2020-08-09 18:35:34 +0800

    调整目录

--- common/src/main/java/com/yanghui/learn/common/Content.java
+++ common/src/main/java/com/yanghui/learn/common/Content.java
@@ -6,7 +6,7 @@ package com.yanghui.learn.common;
 public class Content {
 
     public static void main(String... args){
-        String s = "{\"test\":2}";
+        String s = "<html><head><title>DEMO</title></head><body><h1>你请求uri为：/test</h1><p>5</p></body></html>";
         byte[] bytes = s.getBytes();
         System.out.println("Content-Length: "+bytes.length);
         for(int i = 0; i<bytes.length; i++){
