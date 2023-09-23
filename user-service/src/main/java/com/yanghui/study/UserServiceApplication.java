b091178abc5a84614d5729f5a9d30192a765dcf2
yanghui <yanghuiindex@gmail.com>
2023-09-23 19:45:09 +0800

    springboot启动类加载器

--- user-service/src/main/java/com/yanghui/study/UserServiceApplication.java
+++ user-service/src/main/java/com/yanghui/study/UserServiceApplication.java
@@ -1,6 +1,5 @@
 package com.yanghui.study;
 
-import com.yanghui.learn.common.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,5 +10,16 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+		System.out.println(ClassLoader.getSystemClassLoader());			//sun.misc.Launcher$AppClassLoader@xxx
+		/**
+		 * idea直接启动，和上面一样（当作普通maven项目）；
+		 * 使用spring-boot-maven-plugin打成jar包，java -jar xxx.jar启动，打印org.springframework.boot.loader.LaunchedURLClassLoader@xxx
+		 */
+		System.out.println(Thread.currentThread().getContextClassLoader());
+		/**
+		 * 同上，打印
+		 * org.springframework.boot.loader.LaunchedURLClassLoader@xxx
+		 */
+		System.out.println(UserServiceApplication.class.getClassLoader());
 	}
 }
